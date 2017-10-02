4c93e22d0ce49b17dc17937cf5587662020e93be
Young Jun Park <june0313@gmail.com>
2017-10-02 17:31:10 +0900

    [level 1] 자연수를 뒤집어 리스트 만들기 v1  - while 문과 나머지 연산을 활용함

---
+++ level1/자연수를 뒤집어 리스트로 만들기/task.py
@@ -0,0 +1,13 @@
+def digit_reverse(n):
+    # 함수를 완성해 주세요
+    result = []
+
+    while n is not 0:
+        result.append(n % 10)
+        n //= 10
+
+    return result
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print("결과 : {}".format(digit_reverse(12345)))
