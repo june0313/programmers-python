98e144c716bfb6c992874dacc19e946596a54637
Young Jun Park <june0313@gmail.com>
2017-10-03 11:55:17 +0900

    [level 1] 문자열 다루기 기본  - isdigit() 함수 사용

--- level1/문자열 다루기 기본/string_basic.py
+++ level1/문자열 다루기 기본/string_basic.py
@@ -1,11 +1,6 @@
 def alpha_string46(s):
     # 함수를 완성하세요
-    if len(s) is 4 or len(s) is 6:
-        for c in s:
-            if not ('0' <= c <= '9'):
-                return False
-        return True
-    return False
+    return len(s) in [4, 6] and s.isdigit()
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
