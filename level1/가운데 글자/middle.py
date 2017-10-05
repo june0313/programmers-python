e58139a3f46c134fcda9ccdc1008fb2b366960d2
Young Jun Park <june0313@gmail.com>
2017-10-05 11:32:59 +0900

    [level 1] 가운데 글자 가져오기  - math 모듈 활용

---
+++ level1/가운데 글자/middle.py
@@ -0,0 +1,14 @@
+from math import ceil
+
+
+def string_middle(str):
+    # 함수를 완성하세요
+    return str[(len(str) - 1) // 2:ceil((len(str) - 1) / 2) + 1]
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(string_middle("power"))
+print(string_middle("test"))
+print(string_middle("hello_world"))
+print(string_middle("banana"))
+print(string_middle("rainbow"))
