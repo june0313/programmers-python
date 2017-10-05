9f44420dced2c08eaea77c5901b84324523cea35
Young Jun Park <june0313@gmail.com>
2017-10-05 15:35:48 +0900

    [level 1] 행렬의 덧셈  - zip 함수 활용

--- level1/행렬의 덧셈/sumOfMatrix.py
+++ level1/행렬의 덧셈/sumOfMatrix.py
@@ -1,11 +1,5 @@
 def sumMatrix(A, B):
-    answer = []
-    for i in range(len(A)):
-        sub = []
-        for j in range(len(A[0])):
-            sub.append(A[i][j] + B[i][j])
-        answer.append(sub)
-    return answer
+    return [[c + d for c, d in zip(a, b)] for a, b in zip(A, B)]
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
