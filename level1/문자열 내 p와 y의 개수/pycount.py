4ed374a497f77022f17e366f632147f17343cb02
Young Jun Park <june0313@gmail.com>
2017-10-05 11:14:54 +0900

    [level 1] 문자열 내 p와 y의 개수 v3  - lower (upper) 함수와 count 함수를 활용

--- level1/문자열 내 p와 y의 개수/pycount.py
+++ level1/문자열 내 p와 y의 개수/pycount.py
@@ -1,15 +1,6 @@
 def numPY(s):
     # 함수를 완성하세요
-    count = 0
-
-    for c in s:
-        if c.lower() == 'p':
-            count += 1
-
-        if c.lower() == 'y':
-            count -= 1
-
-    return count is 0
+    return s.lower().count('p') is s.lower().count('y')
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
