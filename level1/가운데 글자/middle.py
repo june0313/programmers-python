4cf1ea64b4053ba0302866ef4cf0f15d3201a692
Young Jun Park <june0313@gmail.com>
2017-10-05 11:34:34 +0900

    [level 1] 가운데 글자 가져오기  - math 모듈 의존성 제거한 코드로 수정

--- level1/가운데 글자/middle.py
+++ level1/가운데 글자/middle.py
@@ -1,9 +1,6 @@
-from math import ceil
-
-
 def string_middle(str):
     # 함수를 완성하세요
-    return str[(len(str) - 1) // 2:ceil((len(str) - 1) / 2) + 1]
+    return str[(len(str) - 1) // 2:len(str) // 2 + 1]
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
