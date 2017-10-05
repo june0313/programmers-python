842adf18b7fe384e522e837be499a3a4426edc26
Young Jun Park <june0313@gmail.com>
2017-10-05 11:10:14 +0900

    [level 1] 문자열 내 p와 y의 개수 v2  - 변수를 하나만 사용하도록 수정

--- level1/문자열 내 p와 y의 개수/pycount.py
+++ level1/문자열 내 p와 y의 개수/pycount.py
@@ -1,16 +1,15 @@
 def numPY(s):
     # 함수를 완성하세요
-    p_count = 0
-    y_count = 0
+    count = 0
 
     for c in s:
         if c.lower() == 'p':
-            p_count += 1
+            count += 1
 
         if c.lower() == 'y':
-            y_count += 1
+            count -= 1
 
-    return p_count == y_count
+    return count is 0
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
