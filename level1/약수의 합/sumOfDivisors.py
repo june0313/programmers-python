fca397d07268bccec31a64a67a3ffa0ee43e2dfa
Young Jun Park <june0313@gmail.com>
2017-10-05 19:10:26 +0900

    [level 1] 약수의 합  - list comprehension 활용

--- level1/약수의 합/sumOfDivisors.py
+++ level1/약수의 합/sumOfDivisors.py
@@ -1,5 +1,5 @@
 def sumDivisor(num):
-    return sum(filter(lambda n: num % n is 0, range(1, num + 1)))
+    return sum([n for n in range(1, num + 1) if num % n is 0])
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
