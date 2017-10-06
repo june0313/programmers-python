7a66234e3ac517443d92b2f6a3f3a979d4bca886
Young Jun Park <june0313@gmail.com>
2017-10-06 14:09:00 +0900

    [level 1] 정수 제곱근 판별하기  - math 모듈의 sqrt 함수 활용

---
+++ level1/정수 제곱근 판별하기/square_root.py
@@ -0,0 +1,10 @@
+import math
+
+
+def nextSqure(n):
+    # 함수를 완성하세요
+    return int(math.sqrt(n)) ** 2 == n and (int(math.sqrt(n)) + 1) ** 2 or 'no'
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print("결과 : {}".format(nextSqure(121)))
