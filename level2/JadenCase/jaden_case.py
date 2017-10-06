d617448ef732c0065f831ad6261c8d232e01692a
Young Jun Park <june0313@gmail.com>
2017-10-07 00:40:49 +0900

    [level 2] JadenCase 문자열 만들기  - list comprehension 활용

--- level2/JadenCase/jaden_case.py
+++ level2/JadenCase/jaden_case.py
@@ -1,6 +1,7 @@
 def Jaden_Case(s):
     # 함수를 완성하세요
-    return ' '.join(map(lambda w: w[0].upper() + w[1:].lower(), s.split()))
+    # return ' '.join(map(lambda w: w[0].upper() + w[1:].lower(), s.split()))
+    return ' '.join([w[0].upper() + w[1:].lower() for w in s.split()])
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
