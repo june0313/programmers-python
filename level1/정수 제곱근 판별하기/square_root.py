1192af3c44826980dc3fc8ac6acb47e383801d29
Young Jun Park <june0313@gmail.com>
2017-10-06 14:13:42 +0900

    [level 1] 정수 제곱근 판별하기  - math 모듈 의존성 제거  - ** 연산자 활용

--- level1/정수 제곱근 판별하기/square_root.py
+++ level1/정수 제곱근 판별하기/square_root.py
@@ -1,9 +1,6 @@
-import math
-
-
 def nextSqure(n):
     # 함수를 완성하세요
-    return int(math.sqrt(n)) ** 2 == n and (int(math.sqrt(n)) + 1) ** 2 or 'no'
+    return int(n ** .5) ** 2 == n and int(n ** .5 + 1) ** 2 or 'no'
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
