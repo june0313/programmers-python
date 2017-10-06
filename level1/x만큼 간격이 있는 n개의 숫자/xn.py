3b1fddf2e6a021b6f9a500cf77e178ca39c87782
Young Jun Park <june0313@gmail.com>
2017-10-06 12:00:04 +0900

    [level 1] x만큼 간격이 있는 n개의 숫자

---
+++ level1/x만큼 간격이 있는 n개의 숫자/xn.py
@@ -0,0 +1,8 @@
+def number_generator(x, n):
+    # 함수를 완성하세요
+    return [i * x for i in range(1, n + 1)]
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(number_generator(3, 5))
+print(number_generator(4, 3))
