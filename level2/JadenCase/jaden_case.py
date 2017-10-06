209fca86593e0ac6b752c4ad1e01ced2398b283f
Young Jun Park <june0313@gmail.com>
2017-10-07 00:36:03 +0900

    [level 2] JadenCase 문자열 만들기  - lambda식 활용

---
+++ level2/JadenCase/jaden_case.py
@@ -0,0 +1,7 @@
+def Jaden_Case(s):
+    # 함수를 완성하세요
+    return ' '.join(map(lambda w: w[0].upper() + w[1:].lower(), s.split()))
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(Jaden_Case("3people unFollowed me for the last week"))
