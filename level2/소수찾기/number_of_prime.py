87eeb2dbc08c89c51d7410c2c4935a851a4c492f
Young Jun Park <june0313@gmail.com>
2017-10-09 23:56:59 +0900

    [level 2] 소수찾기  - 소수 판별 로직을 소수 정의에 맞게 수정

--- level2/소수찾기/number_of_prime.py
+++ level2/소수찾기/number_of_prime.py
@@ -4,9 +4,9 @@ def numberOfPrime(n):
 
 
 def isPrime(n):
-    return len(list(filter(lambda i: n % i is 0, range(1, n + 1)))) is 2
+    return list(filter(lambda i: n % i is 0, range(1, n + 1))) == [1, n]
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
 print(numberOfPrime(10))
-print(numberOfPrime(20))
+print(numberOfPrime(5))
