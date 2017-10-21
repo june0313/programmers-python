d00a8efa1189561d80ec582ca38a8576a699ee60
Young Jun Park <june0313@gmail.com>
2017-10-22 01:39:35 +0900

    [level 4] 최고의 집합  - 만족하는 집합이 없는 경우에 대한 케이스 추가

--- level4/최고의 집합/bestSet2.py
+++ level4/최고의 집합/bestSet2.py
@@ -1,6 +1,7 @@
 def bestSet(n, s):
-    return [s // n] * (n - s % n) + [s // n + 1] * (s % n)
+    return [s // n] * (n - s % n) + [s // n + 1] * (s % n) if n <= s else [-1]
 
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
 print(bestSet(5, 23))
+print(bestSet(16, 5))
